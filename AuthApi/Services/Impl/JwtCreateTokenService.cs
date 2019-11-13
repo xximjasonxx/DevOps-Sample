bac90b16f6e1055bab42048a5ad0a82237b3255b
Jason Farrell <jfarrell@westmonroepartners.com>
2019-11-12 21:17:18 -0600

    Moved Jwt info to config variables

--- AuthApi/Services/Impl/JwtCreateTokenService.cs
+++ AuthApi/Services/Impl/JwtCreateTokenService.cs
@@ -3,28 +3,30 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using AuthApi.Data.Entities;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
 namespace AuthApi.Services.Impl
 {
     public class JwtCreateTokenService : ICreateTokenService
     {
-        private const string JwtKey = "e54186e5-0f36-4faa-bad6-82a00c044ee4";
-        private const string JwtIssuer = "MovieApp";
-        private const string JwtAudience = "MovieApp";
+        private readonly IConfiguration _configuration;
 
         public string CreateToken(User user)
         {
+            if (user == null)
+                throw new ArgumentException(nameof(user));
+
             var claims = new[]
             {
                 new Claim(ClaimTypes.Email, user.EmailAddress)
             };
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtKey));
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtKey"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
 
-            var token = new JwtSecurityToken(JwtIssuer,
-                JwtAudience,
+            var token = new JwtSecurityToken(_configuration["JwtIssuer"],
+                _configuration["JwtAudience"],
                 claims,
                 expires: DateTime.Now.AddMonths(1),
                 signingCredentials: creds);
