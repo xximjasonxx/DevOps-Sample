2e297b03061c37850faea2ef69b12f780d697681
Jason Farrell <jfarrell@westmonroepartners.com>
2019-11-14 14:20:21 -0600

    added seed command for duplicate data check

---
+++ AuthApi/Migrations/20191114201821_DuplicateUserSeed.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuthApi.Migrations
+{
+    public partial class DuplicateUserSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "EmailAddress", "Password" },
+                values: new object[] { new Guid("64093606-828a-4b91-bfa2-0b977ad50f38"), "duplicateuser@test.com", "Yvdksn0nDhJ+ladRQxDyNQ==.Y5fFvB+PZpLmq7MFbMJYGaSgznPYqbosGLPg6uSNyPU=" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("64093606-828a-4b91-bfa2-0b977ad50f38"));
+        }
+    }
+}
