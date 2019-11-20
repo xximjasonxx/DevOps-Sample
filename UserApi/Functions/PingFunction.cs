cb2ec3db7bd21568a63a76adb36c89f59af94b63
Jason Farrell <jfarrell@westmonroepartners.com>
2019-11-20 16:02:38 -0600

    Added initial commit for function

---
+++ UserApi/Functions/PingFunction.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace UserApi.Functions
+{
+    public static class PingFunction
+    {
+        [FunctionName("PingFunction")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "api/v1/ping")] HttpRequest req, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkResult();
+        }
+    }
+}
