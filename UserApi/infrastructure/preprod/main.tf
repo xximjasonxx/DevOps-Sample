b3f99b86db0e2c4c3e2e28d7c693f5f4d193079d
Jason Farrell <jfarrell@westmonroepartners.com>
2019-11-21 20:41:37 -0600

    Added basics for build process

---
+++ UserApi/infrastructure/preprod/main.tf
@@ -0,0 +1,69 @@
+provider "azurerm" {
+  version = "=1.36.0"
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "movieappwmp-rg"
+    storage_account_name = "movieappwmpstate"
+    container_name       = "tfstate"
+    key                  = "userapi-ci-state"
+  }
+}
+
+variable "app_name" {
+  type = "string"
+}
+
+variable "env_name" {
+  type = "string"
+}
+
+variable "jwt_key" {
+  type = "string"
+}
+
+data "azurerm_resource_group" "rg" {
+  name     = "${var.app_name}-rg"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "userapi${var.env_name}storage"
+  resource_group_name      = "${data.azurerm_resource_group.rg.name}"
+  location                 = "${data.azurerm_resource_group.rg.location}"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "storage" {
+  name                  = "userapi-${var.env_name}-artifacts"
+  resource_group_name   = "${data.azurerm_resource_group.rg.name}"
+  storage_account_name  = "${azurerm_storage_account.storage.name}"
+  container_access_type = "private"
+}
+
+data "azurerm_app_service_plan" "plan" {
+  name                = "${var.app_name}-PreProd-ConsumptionPlan"
+  resource_group_name = "${data.azurerm_resource_group.rg.name}"
+}
+
+data "azurerm_container_registry" "registry" {
+  name = "${var.app_name}registry"
+  resource_group_name = "${data.azurerm_resource_group.rg.name}"
+}
+
+resource "azurerm_function_app" "funcApp" {
+    name                       = "${var.app_name}-userapi-${var.env_name}-funcapp"
+    location                   = "WestUS"
+    resource_group_name        = "${data.azurerm_resource_group.rg.name}"
+    app_service_plan_id        = "${azurerm_app_service_plan.plan.id}"
+    storage_connection_string  = "${azurerm_storage_account.storage.primary_connection_string}"
+    version                    = "~2"
+
+    app_settings = {
+        FUNCTIONS_WORKER_RUNTIME                  = "dotnet"
+        WEBSITE_CONTENTAZUREFILECONNECTIONSTRING  = "${azurerm_storage_account.storage.primary_connection_string}"
+        WEBSITE_CONTENTSHARE                      = "${azurerm_storage_account.storage.name}"
+        FUNCTION_APP_EDIT_MODE                    = "readOnly"
+    }
+}
