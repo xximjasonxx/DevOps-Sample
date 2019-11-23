5ac4a3b9994048aba6bc0c11c2e15a3d0e9ae727
Jason Farrell <jfarrell@westmonroepartners.com>
2019-11-22 21:21:03 -0600

    End to End for Zip read

--- UserApi/infrastructure/preprod/main.tf
+++ UserApi/infrastructure/preprod/main.tf
@@ -15,6 +15,10 @@ variable "env_name" {
   type = "string"
 }
 
+variable "artifact_name" {
+  type = "string"
+}
+
 data "azurerm_resource_group" "rg" {
   name     = "${var.app_name}-rg"
 }
@@ -32,6 +36,46 @@ data "azurerm_app_service_plan" "plan" {
   resource_group_name = "${data.azurerm_resource_group.rg.name}"
 }
 
+data "azurerm_storage_account" "storage" {
+  name = "userapiartifactstorage"
+}
+
+data "azurerm_storage_container" "container" {
+  name = "userapi-artifacts"
+}
+
+data "azurerm_storage_account_sas" "storage_sas" {
+ connection_string = "${data.azurerm_storage_account.storage.primary_connection_string}"
+ https_only = false
+
+ resource_types {
+  service = false
+  container = false
+  object = true
+ }
+ 
+ services {
+  blob = true
+  queue = false
+  table = false
+  file = false
+ }
+ 
+ start = "2019–11–21"
+ expiry = "2028–03–21"
+ 
+ permissions {
+  read = true
+  write = false
+  delete = false
+  list = false
+  add = false
+  create = false
+  update = false
+  process = false
+ }
+}
+
 resource "azurerm_function_app" "funcApp" {
     name                       = "userapi-${var.app_name}fa-${var.env_name}"
     location                   = "${data.azurerm_resource_group.rg.location}"
@@ -45,5 +89,8 @@ resource "azurerm_function_app" "funcApp" {
         WEBSITE_CONTENTAZUREFILECONNECTIONSTRING  = "${azurerm_storage_account.storage.primary_connection_string}"
         WEBSITE_CONTENTSHARE                      = "${azurerm_storage_account.storage.name}"
         FUNCTION_APP_EDIT_MODE                    = "readOnly"
+        https_only                                = true
+
+        WEBSITE_RUN_FROM_PACKAGE                  = "${data.azurerm_storage_account.storage.primary_blob_endpoint}userapi-artifacts/${var.artifact_name}${data.azurerm_storage_account_sas.storage_sas.sas}"
     }
 }
