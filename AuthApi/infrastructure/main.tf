e2f7d9cee05890159ac8a1437b6e247d0e63a830
Jason Farrell <jfarrell@westmonroepartners.com>
2019-12-06 13:56:23 -0600

    sql server datasource doesnt support what i thoguht it did

--- AuthApi/infrastructure/main.tf
+++ AuthApi/infrastructure/main.tf
@@ -79,7 +79,7 @@ resource "azurerm_app_service" "authapi" {
     DOCKER_REGISTRY_SERVER_URL      = "${data.azurerm_container_registry.registry.login_server}"
     DOCKER_REGISTRY_SERVER_USERNAME = "${data.azurerm_container_registry.registry.admin_username}"
     DOCKER_REGISTRY_SERVER_PASSWORD = "${data.azurerm_container_registry.registry.admin_password}"
-    ConnectionString                = "Server=${data.azurerm_sql_server.sql.fully_qualified_domain_name};Database=${data.azurerm_sql_database.database.name};User Id=${data.azurerm_sql_server.sql.administrator_login};Password=${data.azurerm_sql_server.sql.administrator_login_password};MultipleActiveResultSets=True;Connection Timeout=60",
+    ConnectionString                = "Server=${data.azurerm_sql_server.sql.fqdn};Database=${azurerm_sql_database.database.name};User Id=${data.azurerm_sql_server.sql.administrator_login};Password=Password01!;MultipleActiveResultSets=True;Connection Timeout=60",
     JwtKey                          = "${var.jwt_key}"
     JwtIssuer                       = "${var.app_name}"
     JwtAudience                     = "${var.app_name}-${var.env_name}"
