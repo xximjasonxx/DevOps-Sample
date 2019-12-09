ccdacfbd0fa116870af91a4ef48e48e0d22365e4
Jason Farrell <jfarrell@westmonroepartners.com>
2019-12-09 17:38:58 -0500

    added return value to usercreatedfunction

--- UserApi/Functions/UserCreatedFunction.cs
+++ UserApi/Functions/UserCreatedFunction.cs
@@ -1,4 +1,5 @@
 
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Azure.WebJobs.Host;
@@ -10,9 +11,9 @@ namespace UserApi.Functions
     public static class UserCreatedFunction
     {
         [FunctionName("UserCreatedFunction")]
-        public static void Run([EventGridTrigger]JObject eventObject, ILogger logger)
+        public static IActionResult Run([EventGridTrigger]JObject eventObject, ILogger logger)
         {
-
+            return new OkObjectResult("Returned");
         }
     }
 }
