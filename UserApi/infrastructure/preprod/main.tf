933d56e0996439e076a60ab30490bad60d31771a
Jason Farrell <jfarrell@westmonroepartners.com>
2019-12-11 21:38:22 -0600

    Commented out subscription creation

--- UserApi/infrastructure/preprod/main.tf
+++ UserApi/infrastructure/preprod/main.tf
@@ -81,16 +81,16 @@ resource "azurerm_function_app" "funcApp" {
     }
 }
 
-resource "azurerm_eventgrid_event_subscription" "default" {
-  name                  = "userCreated-${var.env_name}-subscription"
-  scope                 = "${var.topic_id}"
-  event_delivery_schema = "EventGridSchema"
-  included_event_types  = [ "UserCreatedEvent" ]
-
-  webhook_endpoint {
-    url = "https://${azurerm_function_app.funcApp.default_hostname}/runtime/webhooks/EventGrid?functionName=UserCreatedFunction&code=JMZ0R2zp4qxQdXCPPX4qZ39RM2bQ3kzVj7XFBGvU57uLi8Sl5izMWw=="
-  }
-}
+#resource "azurerm_eventgrid_event_subscription" "default" {
+#  name                  = "userCreated-${var.env_name}-subscription"
+#  scope                 = "${var.topic_id}"
+#  event_delivery_schema = "EventGridSchema"
+#  included_event_types  = [ "UserCreatedEvent" ]
+#
+#  webhook_endpoint {
+#    url = "https://${azurerm_function_app.funcApp.default_hostname}/runtime/webhooks/EventGrid?functionName=UserCreatedFunction&code=JMZ0R2zp4qxQdXCPPX4qZ39RM2bQ3kzVj7XFBGvU57uLi8Sl5izMWw=="
+#  }
+#}
 
 output "funcapp_url" {
   value = "https://${azurerm_function_app.funcApp.default_hostname}"
